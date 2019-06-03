33a21124d4d18f1a7d4e48cc02aef40b1d9a44d5
klaus <oriokiklaus@gmail.com>
2019-06-03 14:02:42 +0300

    add tinymce to applications list

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'watch.apps.WatchConfig',
-    'bootstrap4'
+    'bootstrap4',
+    'tinymce'
 ]
 
 MIDDLEWARE = [
