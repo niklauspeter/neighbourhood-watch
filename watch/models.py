cc0aecdc272a93c5376489dc155fd2ed5e9c02ac
klaus <oriokiklaus@gmail.com>
2019-06-03 13:35:18 +0300

    add tag modelfield to neighbourhood class with a many to many relation

--- watch/models.py
+++ watch/models.py
@@ -2,20 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
-class neighbourhood(models.Model):
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Neighbourhood(models.Model):
     name= models.CharField(max_length=100)
     location= models.CharField(max_length=100)
     occupant_count= models.CharField(max_length=100)
     user= models.ForeignKey(User, max_length=100)
-
-
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.neighbourhood
+        return self.name
 
     def save_neighbourhood(self):
         self.save()
 
     @classmethod
     def delete_neighbourhood(cls,neighbourhood):
-        cls.objects.filter(neighbourhood=neighbourhood).delete()
+        cls.objects.filter(neighbourhood=name).delete()
